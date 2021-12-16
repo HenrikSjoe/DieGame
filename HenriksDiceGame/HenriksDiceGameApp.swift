a93c0a51e4c9d4e53df4c7657b67c53994b842f6
HenrikSjoe <henrik.sjogren@iths.se>
2021-12-16 11:33:57 +0100

    Initial Commit

---
+++ HenriksDiceGame/HenriksDiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HenriksDiceGameApp.swift
+//  HenriksDiceGame
+//
+//  Created by Henrik Sjögren on 2021-12-16.
+//
+
+import SwiftUI
+
+@main
+struct HenriksDiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
